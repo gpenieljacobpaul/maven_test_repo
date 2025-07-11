df320b0eb87fcb185025c0aa947cf6f49fcc4f28
gpenieljacobpaul <penielunlock@gmail.com>
2025-07-12 00:54:29 +0530

    first commit

--- src/main/java/in/jacob/App.java
+++ src/main/java/in/jacob/App.java
@@ -1,13 +1,21 @@
-package in.jacob;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Welcome, " + name + "!");
+
+        System.out.println("\nCounting from 1 to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        scanner.close();
     }
 }
